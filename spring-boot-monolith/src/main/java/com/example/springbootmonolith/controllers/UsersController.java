8c4c5768ca8fa41efde5e547a69139b267ff9788
Tara Fenton <tarafenton@ool-ad02048c.dyn.optonline.net>
2018-05-25 12:03:38 -0400

    User by ID

--- spring-boot-monolith/src/main/java/com/example/springbootmonolith/controllers/UsersController.java
+++ spring-boot-monolith/src/main/java/com/example/springbootmonolith/controllers/UsersController.java
@@ -5,10 +5,12 @@ import com.example.springbootmonolith.models.User;
 import com.example.springbootmonolith.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UsersController {
@@ -19,5 +21,10 @@ public class UsersController {
     @GetMapping("/users")
     public Iterable<User> findAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{userId}")
+    public Optional<User> findUserById(@PathVariable Long userId) {
+        return userRepository.findById(userId);
     }
 }
